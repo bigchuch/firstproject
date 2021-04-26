0b8dd8766c0acfcc445a520afea4a7d65ea449d0
chuch <eajebon@gmail.com>
2021-04-26 12:36:39 +0100

    blog app

--- myapp/apps.py
+++ myapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MyfirstdjangoappConfig(AppConfig):
+class Myapp(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'myfirstdjangoapp'
+    name = 'myapp'
--- myapp/urls.py
+++ myapp/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .import views
+from .views import MyappListView
+
 
 urlpatterns = [
-    path('index/',views.index,name = "index")
+    path('', MyappListView.as_view(), name = 'home')
 ]
